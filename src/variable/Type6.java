6b97700b5b87ccf5c5acaeabd798f6f2e58a02cf
jiwonp6 <pwldnjs2806@naver.com>
2024-02-07 14:02:57 +0900

    variable practice - promotion, casting #5(240207)

---
+++ src/variable/Type6.java
@@ -0,0 +1,12 @@
+package variable;
+//#2-14 문자열 연산 자동 타입 변환(2024-02-07)
+public class Type6 {
+    public static void main(String[] args) {
+        // "문자열" + 숫자 => "문자열숫자"
+        // "숫자" + 문자열=> "숫자문자열"
+        System.out.println( 3 + 7 );    // 10
+        System.out.println( "3" + 7 );  // "37"
+        System.out.println( 3 + "7" );  // "37"
+
+    }
+}
