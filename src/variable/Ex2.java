b1d563af5269cb40577ca1c44ffa0ac2ead8ef58
jiwonp6 <pwldnjs2806@naver.com>
2024-02-08 12:08:07 +0900

    operator - Ex (240208)

--- src/variable/Ex2.java
+++ src/variable/Ex2.java
@@ -3,7 +3,7 @@ package variable;
 public class Ex2 {
     public static void main(String[] args) {
         // 변수 n1, n2를 선언하고 각각 30, 40을 할당하세요.
-        // 두 변수의 합으 ㄹ저장하는 sum 변수를 선언하세요.
+        // 두 변수의 합을 저장하는 sum 변수를 선언하세요.
         // sum을 출력하세요.
 
         int n1 = 30;
